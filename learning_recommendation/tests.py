b32c5b67cbac8673cde7a78792386aae944bf0ff
Chiama <root@Sagebook.local>
2021-10-05 03:05:20 +0300

    Removed API Test Case in class

--- learning_recommendation/tests.py
+++ learning_recommendation/tests.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
-from rest_framework.test import APITestCase
 
 # Create your tests here.
 
 
-class predictTestCase(APITestCase):
+class predictTestCase():
 
     def test_prediction_correct(self):
         data = {
